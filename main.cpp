f06b93d49c94591ea00cb1e477cdf80d95f81b75
Hatim Rachid <hat83203@gmail.com>
2023-01-19 21:14:21 +0100

    shader chapter using uniforms

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include <iostream>
 #include "WindowManager.h"
 #include "InputManager.h"
-
+#include <math.h>
 int main(void)
 {
 
 	GLFWwindow *window;
-	unsigned int VBO,VAO, vertexShader, fragmentShader, shaderProgram;
+	unsigned int VBO, VAO, vertexShader, fragmentShader, shaderProgram;
 	// store shadeer info
 	int success;
 	char infoLog[512];
@@ -24,11 +24,10 @@ int main(void)
 	InputManager inputMG(window);
 	winMG.loadGlad();
 	winMG.setResizeEvent();
-	//generating 1 vertex array object so store all vbo and attrib pointer config with id vao
-	glGenVertexArrays(1,&VAO);
-	//binding vertex array
+	// generating 1 vertex array object so store all vbo and attrib pointer config with id vao
+	glGenVertexArrays(1, &VAO);
+	// binding vertex array
 	glBindVertexArray(VAO);
-
 
 	// generating 1  buffer that has id VBO
 	glGenBuffers(1, &VBO);
@@ -36,19 +35,19 @@ int main(void)
 	// VERTEX BUFFER OBJECT is an object that can store data and sent id to the GPU Memory
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// copying data to the BUFFER
-	//send data to gpu
+	// send data to gpu
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// third parameter of glBufferData
 	// GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
 	// GL_STATIC_DRAW: the data is set only once and used many times.
 	// GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
 	// configuring how data should be interpreted from the gpu
-	glVertexAttribPointer(0, 3,GL_FLOAT,GL_FALSE, 3 * sizeof(float), (void *)0);
-	//enabling the location of it
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	// enabling the location of it
 	glEnableVertexAttribArray(0);
-	//unbound any vertex array for later user
+	// unbound any vertex array for later user
 	glBindVertexArray(0);
-	//fragment shader and vertex shader source code
+	// fragment shader and vertex shader source code
 	const char *vertexShaderSource = "#version 330 core\n"
 									 "layout (location = 0) in vec3 aPos;\n"
 									 "void main()\n"
@@ -57,9 +56,10 @@ int main(void)
 									 "}\0";
 	const char *fragmentShaderSource = "#version 330 core\n"
 									   "out vec4 FragColor\n;"
+									   "uniform vec4 ourColor; \n"
 									   "void main()\n"
 									   "{\n"
-									   "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+									   "FragColor = ourColor;\n"
 									   "}\0";
 	// creating vertex shader and handling errors
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -84,7 +84,7 @@ int main(void)
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
 				  << infoLog << std::endl;
 	}
-	//shaders need to be linked with a shader program for later use
+	// shaders need to be linked with a shader program for later use
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
@@ -96,17 +96,23 @@ int main(void)
 		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n"
 				  << infoLog << std::endl;
 	}
-	//unbounding any shader program
+	// unbounding any shader program
 	glUseProgram(0);
-	//liberating ressources
+	// liberating ressources
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-	//binding the vertex array and shader program
-	glBindVertexArray(VAO);
+	// binding the vertex array and shader program
 	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
 
+std::cout << "yes\n";
 	while (!glfwWindowShouldClose(window))
 	{
+		float timeVaue = glfwGetTime();
+		float sinValue = sin(timeVaue);
+		int vertexUniformLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexUniformLocation, 0.0f, sinValue, sinValue, 1.0f);
+
 		// process input
 		inputMG.registerKeyPress([](GLFWwindow *win) -> void
 								 { glfwSetWindowShouldClose(win, true); },
@@ -114,13 +120,14 @@ int main(void)
 		// rendering goes here
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		//draawing with peremetive GL_TRIANGLE starting for vertex 0 and draw 3 verticies
-		glDrawArrays(GL_TRIANGLES,0,3);
+		// draawing with peremetive GL_TRIANGLE starting for vertex 0 and draw 3 verticies
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		// swap double buffres
 		glfwSwapBuffers(window);
 		// check for events
 		glfwPollEvents();
 	}
+
 
 	return 0;
 }
